1b2e2413064637c445762fc14df966aa358f86e5
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-12-14 20:59:41 -0700

    Add ability to select activities and adjust total cost based on selections

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ window.addEventListener('DOMContentLoaded', () => {
 	const otherJobRoleField = document.querySelector('#other-job-role');
 	const shirtDesignSelect = document.querySelector('#design');
 	const shirtColorSelect = document.querySelector('#color');
+	const activitiesDiv = document.querySelector('.activities-box');
+	const activitiesCost = document.querySelector('.activities-cost');
 
 	// Apply focus to name field
 	nameField.focus();
@@ -17,17 +19,6 @@ window.addEventListener('DOMContentLoaded', () => {
 	toggleShirtColorSelect(true);
 
 	// Helper functions
-	function handleJobSelectChange(evt) {
-		const { value } = evt.target;
-		if (value === 'other') toggleOtherJobInput('');
-		else toggleOtherJobInput('none');
-	}
-
-	function handleDesignSelectChange(evt) {
-		const { value } = evt.target;
-		updateShirtColorOptions(value);
-	}
-
 	function updateShirtColorOptions(design) {
 		const shirtColorOptions = shirtColorSelect.querySelectorAll(
 			'option[data-theme]'
@@ -48,8 +39,45 @@ window.addEventListener('DOMContentLoaded', () => {
 		shirtColorSelect.disabled = isDisabled;
 	}
 
+	function updateAvailableActivities(selectedActivity) {
+		const activities = activitiesDiv.querySelectorAll('input[type="checkbox"]');
+		const date = selectedActivity.getAttribute('data-day-and-time');
+		activities.forEach((activity) => {
+			const activityDate = activity.getAttribute('data-day-and-time');
+			if (date === activityDate) activity.disabled = true;
+			else activity.disabled = false;
+		});
+	}
+
+	function updateTotalCost(activity) {
+		const activityCost = parseInt(activity.dataset.cost);
+		let currentTotal = parseInt(activitiesCost.textContent.replace(/^\D+/, ''));
+		if (activity.checked) currentTotal += activityCost;
+		else currentTotal -= activityCost;
+		activitiesCost.textContent = `Total: $${currentTotal}`;
+	}
+
+	// Callback functions
+	function handleJobSelectChange(evt) {
+		const { value } = evt.target;
+		if (value === 'other') toggleOtherJobInput('');
+		else toggleOtherJobInput('none');
+	}
+
+	function handleDesignSelectChange(evt) {
+		const { value } = evt.target;
+		updateShirtColorOptions(value);
+	}
+
+	function handleActivityChange(evt) {
+		const { target } = evt;
+		console.log(target);
+		updateAvailableActivities(target);
+		updateTotalCost(target);
+	}
+
 	// Event listeners
 	jobRoleSelect.addEventListener('change', handleJobSelectChange);
-
 	shirtDesignSelect.addEventListener('change', handleDesignSelectChange);
+	activitiesDiv.addEventListener('change', handleActivityChange);
 });
